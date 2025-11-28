3ad4eac1bbe993a6750d1c3d0c696bbe87fb0d8a
AnsariChand <YOUR_EMAIL>
2025-11-28 16:36:51 +0530

    UseStates

---
+++ FirstApp/app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text,  } from "react-native";
+
+export function index() {
+  return(
+    <View>
+      <Text style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        Its'me Mario!
+      </Text>
+
+    </View>
+  );
+}
